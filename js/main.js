c9c761d9926c6be2576c732e6c67bdd700d8fe31
Alan Castro <alanarturocastrooo@gmail.com>
2026-01-13 15:55:12 -0600

    CV web

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const cards = document.querySelectorAll('.project-card');
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+
+let index = 0;
+
+function showCard(i) {
+  cards.forEach(card => card.classList.remove('active'));
+  cards[i].classList.add('active');
+}
+
+nextBtn.addEventListener('click', () => {
+  index = (index + 1) % cards.length;
+  showCard(index);
+});
+
+prevBtn.addEventListener('click', () => {
+  index = (index - 1 + cards.length) % cards.length;
+  showCard(index);
+});
+
